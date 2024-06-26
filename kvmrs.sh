0442a0f4183b17ade4f8cf7912c0147a56de5ec7
chromefinch <chromefinch@users.noreply.github.com>
2024-06-26 09:20:17 -0400

    Update kvmrs.sh

--- kvmrs.sh
+++ kvmrs.sh
@@ -8,6 +8,9 @@ if [[ $EUID -ne 0 ]]; then
 fi
 echo "This VM restore script assumes you previously backed up with kvmbk.sh and that you are using .qcow2 or .img disks"
 echo ""
+echo "This should be ran from the same folder as the backup"
+echo ""
+echo ""
 userid=$SUDO_USER
 echo "current dir"
 pwd
@@ -15,24 +18,27 @@ multidrive () {
 	echo "";
 	echo Multiple drive paths in xml;
 	echo reading xml drives...;
-	drives=$(cat $vmname.xml| grep -Eo "'.*\.qcow2|\.img"| sed "s/'//g");
+	drives=$(cat $foundname.xml| grep -Eo "'.*\.qcow2|\.img"| sed "s/'//g");
 	echo $drives;
 	count=1;
 	for drive in $drives; do
 		ndest=$(echo $drive| grep -Eo ".*\/");
 		ndrive=${drive##*/}
+		newdrive=$(echo $ndrive| sed -z "s/$foundname/$vmname/g")
 		echo ""
 		read -p "please enter a drive path for $ndrive [$ndest]: " qdest
 		fdest=${qdest:-$ndest}
-		echo VM $vmname drive $ndrive is being copied to $fdest
+		echo VM $vmname drive $ndrive.panic is being copied to $fdest, rename to follow
 		test $fdest&&echo 'Path already exists, proceeding...'||mkdir -p $fdest
-		echo rsync --info=progress2 $ndrive $fdest
-		echo Updating xml
+		echo rsync --info=progress2 -b --suffix=".panic" $ndrive $fdest
+		echo renaming drive from $ndrive.panic to $newdrive
+		sudo mv $fdest$ndrive.panic $fdest$newdrive
+		echo Updating xml with path info
 		sed -z -i "s|$ndest/$vmname|$fdest/$vmname|g" $vmname.xml
 		((count++))
 	done;
 }
-sleep 3
+sleep 2
 read -p "Where are backup files? (absolute path only) " bkpath
 echo "Listing found backup files:"
 ls -lah $bkpath
@@ -67,25 +73,27 @@ case $yn in
 	    vmname=${qvmname:-$foundname};
 	    echo "permanent changes imminent!";
 	    sleep 5 ;
-	    echo "Renaming system name in xml and for disks...";
+	    echo "Backing up & copying xml for new system config...";
 	    cp $foundname.xml $foundname.xml.bk;
-	    sed -z -i "s/$foundname/$vmname/g" $foundname.xml;
+		cp $foundname.xml $vmname.xml;
+	    sed -z -i "s/$foundname/$vmname/g" $vmname.xml;
+	    echo generating a new uuid for vm, this step is important.
 	    newuuid=$(uuidgen);
-	    sed -z -i "s|<uuid>.*</uuid>|<uuid>$newuuid</uuid>|g" $foundname.xml;
-	    rename s/$foundname/$vmname/g * ;
+	    sed -z -i "s|<uuid>.*</uuid>|<uuid>$newuuid</uuid>|g" $vmname.xml;
 		destcount=$(cat $vmname.xml| grep -Eo "'\S+\.qcow2"| sed "s/'//g" | grep -Eo "\S+\/" | sort -u| wc -l);
 		if [[ $destcount -eq "1" ]] ; then
 			dest=$(cat $vmname.xml| grep -Eo "'\S+\.qcow2|\.img"| sed "s/'//g" | grep -Eo "\S+\/" | sort -u);
 			read -p "Where would you like to restore disks to [$dest]? " qdest;
 			dst=${qdest:-$dest};
 			test $dst&&echo 'Path already exists, proceeding'||mkdir -p $dst;
-			drives=$(ls -lah $bkpath | grep -Eo "$vmname\S+" | grep -Ev ".xml");
+			drives=$(ls -lah $bkpath | grep -Eo "$foundname\S+" | grep -Ev ".xml");
 			for drive in $drives; do
+				newdrive=$(echo $drive| sed -z "s/$foundname/$vmname/g")
 				echo " "
-				echo VM $vmname drive $drive is being copied to $dst
-				rsync --info=progress2 $drive $dst
-				echo Updating xml
-				sed -z -i "s|$ndest/$vmname|$fdest/$vmname|g" $vmname.xml
+				echo VM $vmname drive $drive.panic is being copied to $dst, rename to follow
+				rsync --info=progress2 -b --suffix=".panic" $drive $dst
+				echo renaming drive from $drive.panic to $newdrive
+				sudo mv $dst$drive.panic $dst$newdrive
 			done
 		else
 			multidrive
