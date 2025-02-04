76646442cd3e918e7b2281f37096b1ddfb34af8d
chromefinch <38330372+chromefinch@users.noreply.github.com>
2025-02-03 21:13:06 -0500

    Update kali_Install.sh

--- kali_Install.sh
+++ kali_Install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 purple='\033[0;35m'
-clear='\033[0m'
-echo -e ${purple}
+CL=$(echo "\033[m")
+function header_info {
+    clear
+    echo -e ${purple}
 cat <<'EOF'
 .............. 
             ..,;:ccc,.
@@ -24,18 +26,79 @@ cat <<'EOF'
                                             c
                                             .'
                                              .
-version 1.6
 EOF
-echo -e ${clear}
-print_red (){
-	echo -e "\033[0;31m$1\033[0m"
+echo -e ${CL}
+echo "version 2.0"
 }
+YW=$(echo "\033[33m")
+BL=$(echo "\033[36m")
+RD=$(echo "\033[01;31m")
+GN=$(echo "\033[1;92m")
+BFR="\\r\\033[K"
+HOLD="-"
+CM="${GN}✓${CL}"
+silent() { "$@" >/dev/null 2>&1; }
+set -e
+header_info
+echo "Loading..."
+function msg_info() {
+  local msg="$1"
+  echo -ne " ${HOLD} ${YW}${msg}..."
+}
+
+function msg_ok() {
+  local msg="$1"
+  echo -e "${BFR} ${CM} ${GN}${msg}${CL}"
+}
+
+set -eu -o pipefail # fail on error and report it, debug all lines
+print_red (){
+echo -e "\033[0;31m$1\033[0m"
+}   
 if [[ $EUID -ne 0 ]]; then
-   print_red "This script must be run as root"
-   exit 1
+    print_red "This script must be run as root"
+    exit 1
 fi
-read -p "Do you want to roll keys? (y/N) " yn
-case $yn in
+userid=$SUDO_USER
+
+if ! hostnamectl | grep -Eq "Operating System: Kali GNU/Linux Rolling"; then
+  echo -e "This version of Linux is not supported"
+  echo -e "Requires Kali GNU/Linux Rolling "
+  echo -e "Exiting..."
+  sleep 2
+  exit
+fi
+
+install() {
+    STD="silent"
+    header_info
+    read -p "Would you like to reboot at the end of this script?  (y/N) " rebootq
+    header_info
+    msg_info "Launching no touch in - 5"
+    sleep 1
+    msg_info "4"
+    sleep 1
+    msg_info "3"
+    sleep 1
+    msg_info "2"
+    sleep 1
+    msg_info "1"
+    sleep 1
+    msg_ok "Launched"
+
+    mscode
+    letsUpdate
+    apps
+    tmuxStuff
+    chromeInstall
+    nvidiaInstall
+    xrdpInstall
+    rando
+    allDone
+}
+
+rollKeys() {
+case $keyq in
     [yY] ) echo "ok, here we go!" ;
             #Move old SSH keys into /etc/ssh/old_keys/ dir, issue new SSH keys, verify that the hashes do not match by taking MD5 sums of the old keys and comparing to the MD5 sums of the new keys via a diff. Error checking is incorporated to determine if the old_ssh_signatures file was not created (if not created, this indicates an error in generating the old_keys dir or moving old keys into this new dir). Finally, if something goes wrong and the new keys were not generated (diff does not indicate 'differ' with -q flag) the script will warn the user and require them to manually update ssh keys. If they decline the script will exit.
             test -d /etc/ssh/old_keys&&echo backup folder already exists, deleting...&&sudo rm -rf /etc/ssh/old_keys&&sudo mkdir /etc/ssh/old_keys||sudo mkdir /etc/ssh/old_keys
@@ -69,83 +132,38 @@ case $yn in
             fi;;
     * ) echo "Probably fine";;
 esac
+}
 
-set -eu -o pipefail # fail on error and report it, debug all lines
+letsUpdate() {
+    msg_info "Updating before install"
+    $STD sudo apt-get update
+    msg_ok "apt update done"
+    msg_info "doing upgrade"
+    $STD sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
+    msg_ok "apt upgrade done"
+    msg_info "doing dist-upgrade"
+    $STD sudo DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y
+    msg_ok "dist-upgrade done"
+    msg_info  "doing apt autoremove"
+    $STD sudo DEBIAN_FRONTEND=noninteractive apt-get autoremove -y
+    msg_ok "apt autoremove done"
+    msg_info  "doing autoclean"
+    $STD sudo DEBIAN_FRONTEND=noninteractive apt-get autoclean -y
+    msg_ok "apt autoclean done"
+    msg_info "apt fix broken"
+    $STD sudo DEBIAN_FRONTEND=noninteractive apt --fix-broken install -y 
+    msg_ok "apt fix done"
+    msg_info "updating searchsploit"
+    $STD sudo searchsploit -u  && echo -e "\n"
+    msg_ok "searchsploit updated"
+    msg_info "running update one more again"
+    $STD sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
+    msg_ok "apt update done"
+    msg_ok "Updates done"
+}
 
-userid=$SUDO_USER
-cd /home/$userid
-echo $(pwd)
-
-read -p "Do you want some Nvidia? This installs cuda for Hashcat. (y/N) " nvidiainstall
-
-read -p "Do you want to run lynis? (y/N) " lynisrun
-
-cd /home/$userid/Downloads
-
-read -p "Do you want to install Google Chrome? (Y/n) " chromeinstall
-
-read -p "Will you be using xrdp? (Y/n) " xrdpinstall
-
-echo Updating before install... 
-echo -e "\n"
-sudo apt update  >> /dev/null 2>&1
-echo "...doing full-upgrade..."
-echo -e "\n"
-sudo apt full-upgrade
-echo "....doing upgrade...."
-echo -e "\n"
-sudo apt update && sudo apt upgrade -y
-echo ".....doing dist-upgrade....."
-echo -e "\n"
-sudo DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y  >> /dev/null 2>&1
-echo  "......doing autoremove......"
-echo -e "\n"
-sudo DEBIAN_FRONTEND=noninteractive apt-get autoremove -y  >> /dev/null 2>&1
-echo  ".......doing autoclean......."
-echo -e "\n"
-sudo DEBIAN_FRONTEND=noninteractive apt-get autoclean -y  >> /dev/null 2>&1
-echo "........Fixing Broken Packages........"
-echo -e "\n"
-sudo DEBIAN_FRONTEND=noninteractive apt --fix-broken install -y  >> /dev/null 2>&1
-echo ".........updating searchsploit........."
-sudo searchsploit -u && echo -e "\n"
-echo -e "\n"
-sudo DEBIAN_FRONTEND=noninteractive apt-get update  >> /dev/null 2>&1
-echo Updates finished, starting installs
-echo you have 3 seconds to proceed ...
-echo or
-echo hit Ctrl+C to quit
-echo -e "\n"
-sleep 4
-
-echo adding update alias and terminal candy
-term=".$(echo $SHELL | grep -Eo 'bash|zsh')rc"
-sudo sed -z -i 's|HISTSIZE=1000\nHISTFILESIZE=2000\n|HISTSIZE=1000000\nHISTFILESIZE=2000000\nexport HISTFILE=~/.bash_history\n|g' /home/$userid/$term
-sudo sed -z -i "s|alias ll='ls -alF'\n|alias ll='ls -alFh'\n|g" /home/$userid/$term
-sudo sed -z -i "s|alias ll='ls -l'\n|alias ll='ls -alFh'\n|g" /home/$userid/$term
-sudo sed -z -i 's|HISTSIZE=1000\nSAVEHIST=2000\n|HISTSIZE=1000000\nSAVEHIST=2000000\n|g' /home/$userid/$term
-alreadythere="$(tail -n 1 /home/$userid/$term)"
-testv="#alreadydoneflag"
-if [ "$alreadythere" = "$testv" ]; then
-  echo "bashrc already updated"
-else
-  cat << EOF >> /home/$userid/$term
-fastfetch
-alias update="sudo apt update && sudo apt upgrade -y && sudo searchsploit -u"
-alias netrset="sudo service networking restart && ip -br a"
-#alreadydoneflag
-EOF
-  sudo chown $userid:$userid /home/$userid/$term
-fi
-
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-sudo rm -f packages.microsoft.gpg
-
-
-echo installing the must-have pre-requisites like flatpack and the like
-while read -r p ; do sudo DEBIAN_FRONTEND=noninteractive apt-get install -y $p  >> /dev/null 2>&1 && echo -e "\n" $p installed... "\n" ; done < <(cat << "EOF"
+apps() {
+    while read -r p ; do $STD sudo DEBIAN_FRONTEND=noninteractive apt-get install -y $p && msg_ok "$p installed" ; done < <(cat << "EOF"
     bpytop
     flatpak
     code
@@ -158,6 +176,7 @@ while read -r p ; do sudo DEBIAN_FRONTEND=noninteractive apt-get install -y $p  
     pipx
     git
     seclists
+    fastfetch
     netexec
     feroxbuster
     gowitness
@@ -170,43 +189,94 @@ while read -r p ; do sudo DEBIAN_FRONTEND=noninteractive apt-get install -y $p  
     mingw-w64
 EOF
 )
+    msg_info "Installing flathub"
+    $STD sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    msg_ok "Flathub installed"
+    while read -r p ; do $STD sudo flatpak install --or-update flathub $p -y --noninteractive && msg_ok "flathub $p installed" ; done < <(cat << "EOF"
+    md.obsidian.Obsidian
+    org.onlyoffice.desktopeditors
+    com.jetbrains.PyCharm-Community
+    com.github.tchx84.Flatseal
+EOF
+)
+#$STD sudo gzip -d /usr/share/wordlists/rockyou.txt.gz
+}
 
-sudo DEBIAN_FRONTEND=noninteractive apt --fix-broken install -y
+mscode(){
+    msg_info "Adding Microsoft Visual Studio Code repo"
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | $STD sudo gpg --dearmor > packages.microsoft.gpg
+    $STD sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+    echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | $STD sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+    $STD sudo rm -f packages.microsoft.gpg
+    msg_ok "Visual Studio Code repo added"
+}
 
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-sudo flatpak install --or-update flathub md.obsidian.Obsidian -y
-sudo flatpak install --or-update flathub org.onlyoffice.desktopeditors -y
-sudo flatpak install --or-update flathub com.jetbrains.PyCharm-Community -y
-sudo flatpak install --or-update flathub com.github.tchx84.Flatseal -y
+tmuxStuff() {
+  msg_info "tmux fun"
+  cd /home/$userid/
+  test -d /home/$userid/.tmux.bk&&sudo rm -rf /home/$userid/.tmux.bk
+  test -d /home/$userid/.tmux.conf.local.bk&&sudo rm -rf /home/$userid/.tmux.conf.local.bk
+  test -d /home/$userid/.tmux&&sudo mv -f /home/$userid/.tmux /home/$userid/.tmux.bk
+  test -d /home/$userid/.tmux.conf.local&&sudo mv -f /home/$userid/.tmux.conf.local /home/$userid/.tmux.conf.local.bk
+  git clone -q https://github.com/gpakosz/.tmux.git
+  ln -s -f .tmux/.tmux.conf
+  cp .tmux/.tmux.conf.local /home/$userid/
+  sudo chown $userid:$userid /home/$userid/.tmux
+  sudo chown $userid:$userid /home/$userid/.tmux.conf.local
+  msg_ok "tmux fun installed"
 
-echo tmux fun
-cd /home/$userid/
-test -d /home/$userid/.tmux.bk&&sudo rm -rf /home/$userid/.tmux.bk||echo ...
-test -d /home/$userid/.tmux.conf.local.bk&&sudo rm -rf /home/$userid/.tmux.conf.local.bk||echo ....
-test -d /home/$userid/.tmux&&sudo mv -f /home/$userid/.tmux /home/$userid/.tmux.bk||echo .....
-test -d /home/$userid/.tmux.conf.local&&sudo mv -f /home/$userid/.tmux.conf.local /home/$userid/.tmux.conf.local.bk||echo ......
-git clone https://github.com/gpakosz/.tmux.git
-ln -s -f .tmux/.tmux.conf
-cp .tmux/.tmux.conf.local /home/$userid/
-sudo chown $userid:$userid /home/$userid/.tmux
-sudo chown $userid:$userid /home/$userid/.tmux.conf.local
+  msg_info "adding update alias and terminal candy"
+  term=".$(echo $SHELL | grep -Eo 'bash|zsh')rc"
+  sudo sed -z -i 's|HISTSIZE=1000\nHISTFILESIZE=2000\n|HISTSIZE=1000000\nHISTFILESIZE=2000000\nexport HISTFILE=~/.bash_history\n|g' /home/$userid/$term
+  sudo sed -z -i "s|alias ll='ls -alF'\n|alias ll='ls -alFh'\n|g" /home/$userid/$term
+  sudo sed -z -i "s|alias ll='ls -l'\n|alias ll='ls -alFh'\n|g" /home/$userid/$term
+  sudo sed -z -i 's|HISTSIZE=1000\nSAVEHIST=2000\n|HISTSIZE=1000000\nSAVEHIST=2000000\n|g' /home/$userid/$term
+  alreadythere="$(tail -n 1 /home/$userid/$term)"
+  testv="#alreadydoneflag"
+  if [ "$alreadythere" = "$testv" ]; then
+    msg_ok "bashrc already updated"
+  else
+    cat << EOF >> /home/$userid/$term
+fastfetch
+alias update="sudo apt update && sudo apt upgrade -y && sudo searchsploit -u"
+alias netrset="sudo service networking restart && ip -br a"
+#alreadydoneflag
+EOF
+    sudo chown $userid:$userid /home/$userid/$term
+    msg_ok "added update alias and terminal candy"
+  fi
+}
 
-ff="fastfetch"
-fastfile='fastfetch-linux-amd64.deb'
-echo downloading $ff...
-test -f /home/$userid/Downloads/$fastfile&&echo $ff' already downloaded'||wget 'https://github.com/fastfetch-cli/fastfetch/releases/download/2.15.0/fastfetch-linux-amd64.deb' -P /home/$userid/Downloads
-which "$ff" | grep -o "$ff" > /dev/null &&  echo $ff' already installed' || sudo dpkg -i /home/$userid/Downloads/$fastfile
+nvidiaInstall() {
+    msg_info "Assessing Nvidia HW status"
+    if ! lspci | grep -q "VGA compatible controller: NVIDIA"; then
+        msg_ok "No Nvidia card found, skipping"
+    else
+        $STD sudo DEBIAN_FRONTEND=noninteractive apt install -y nvidia-cuda-toolkit
+        msg_ok "Nvidia cuda toolkit installed"
+    fi
+}
 
-case $xrdpinstall in
-        [nN] ) echo "Skipping xrdp configs";;
-        * ) echo "Updating xrdp confgs to Gnome Desktop & non standard port";
-            #change rdp server port so responder will not conflict, you will still need to enable the service;
-            echo xrdp port changed to 3390;
-            sed -i 's/port=3389/port=3390/g' /etc/xrdp/xrdp.ini;
-            sudo apt install -y kali-desktop-gnome;
-            echo "gnome-session" | tee /home/$userid/.xsession;;
-esac
+chromeInstall() {
+  msg_info "installing Google Chrome"
+  cd /home/$userid/Downloads
+  chromedefault='google-chrome-stable_current_amd64.deb'
+  #read -p "Enter desired version of Google Chrome [$chromedefault]:" chromev;
+  chromefile=${chromev:-$chromedefault}
+  #echo downloading $chromefile...
+  test -f /home/$userid/Downloads/$chromefile&&msg_ok 'Chrome already downloaded'||wget -q https://dl.google.com/linux/direct/$chromefile -P /home/$userid/Downloads
+  gc="google-chrome"
+  which "$gc" | grep -o "$gc" > /dev/null &&  msg_ok $gc' already installed' || $STD sudo dpkg -i /home/$userid/Downloads/$chromefile && msg_ok "Google Chrome installed"
 
+}
+
+xrdpInstall() {
+    msg_info "Updating xrdp confgs to Gnome Desktop & non standard port"
+    #change rdp server port so responder will not conflict, you will still need to enable the service
+    sed -i 's/port=3389/port=3390/g' /etc/xrdp/xrdp.ini
+    $STD sudo apt install -y kali-desktop-gnome
+    echo "gnome-session" >> /home/$userid/.xsession
+    msg_ok "xrdp isntalled on port 3390 but service is not enabled/started"
 #fixes xrdp color prompt
 cat << EOF > /etc/polkit-1/rules.d/02-allow-colord.rules
 polkit.addRule(function(action, subject) {
@@ -221,47 +291,106 @@ return polkit.Result.YES;
 }
 });
 EOF
+}
 
-case $nvidiainstall in
-    [yY] ) echo ok, installing cuda;
-        sudo apt install -y linux-headers-amd64;
-        sudo apt install -y nvidia-driver nvidia-cuda-toolkit;;
-    * ) echo Skipping Nvidia Cuda install;;
-esac
+rando() {
+    msg_info "Randomizing machine name"
+    old=$(cat /etc/hostname)
+    array=()
+    for i in {A..Z} {0..9}; 
+        do
+        array[$RANDOM]=$i
+    done
+    stubsthename=$(printf %s ${array[@]::12})
+    # Quote the variables to handle potential spaces or special characters in the hostname
+    $STD sudo sed -i "s/$old/$stubsthename/g" /etc/hosts  # Use \< and \> for word boundaries
+    $STD sudo sed -i "s/$old/$stubsthename/g" /etc/hostname # Use \< and \> for word boundaries
+    $STD sudo hostname "$stubsthename"
+    msg_ok "Renamed $old to $stubsthename"
+    #$STD sudo systemctl restart networking
+}
 
-case $lynisrun in
-    [yY] ) echo "Running lynis. Please wait...forever...";
-        sudo lynis audit system > /home/$userid/lynis_log.txt;
-        sudo netstat -tulpn > /home/$userid/open_ports_log.txt;
-	sudo gzip -d /usr/share/wordlists/rockyou.txt.gz;;
-    * ) echo "Skipping lynis run";;
-esac
+lynisrun() {
+    msg_info "Running lynis. Please wait...forever..."
+    sudo lynis audit system > /home/$userid/lynis_log.txt
+    sudo netstat -tulpn > /home/$userid/open_ports_log.txt
+}
 
-case $chromeinstall in
-        [nN] ) echo "Skipping Google Chrome install";;
-        * ) echo cd /home/$userid/Downloads;
-            chromedefault='google-chrome-stable_current_amd64.deb';
-            #read -p "Enter desired version of Google Chrome [$chromedefault]:" chromev;
-            chromefile=${chromev:-$chromedefault};
-            echo downloading $chromefile...;
-            test -f /home/$userid/Downloads/$chromefile&&echo 'Chrome already downloaded'||wget https://dl.google.com/linux/direct/$chromefile -P /home/$userid/Downloads;
-            gc="google-chrome";
-            which "$gc" | grep -o "$gc" > /dev/null &&  echo $gc' already installed' || sudo dpkg -i /home/$userid/Downloads/$chromefile;;
-esac
-### Hostname randomizer  - 12/17/2024 ###
+allDone() {
+  msg_ok "Ok, I think we're done!\n"
+  case $rebootq in 
+    [yY]) echo "install app icons taskbar by visiting the following link: https://extensions.gnome.org/extension/4944/app-icons-taskbar/"
+        sleep 3
+        sudo reboot
+        return 1
+        ;;
+    *) msg_info "you should restart soonest"
+        echo " install app icons taskbar by visiting the following link: https://extensions.gnome.org/extension/4944/app-icons-taskbar/"
+        exit
+        ;;
+  esac
+}
 
-old=$(cat /etc/hostname)
-new=$(tr -dc 'A-Z0-9' < /dev/urandom | head -c12)
-sed -i "s/$old/$new/g" /etc/hosts
-sed -i "s/$old/$new/g" /etc/hostname
-hostname "$new"
+custom() {
+    header_info
+    read -r -p "Verbose mode? <y/N> " prompt
+    if [[ ${prompt,,} =~ ^(y|yes)$ ]]; then
+    STD=""
+    else
+    STD="silent"
+    fi
+    header_info
+    read -p "Would you like to reboot at the end of this script?  (y/N) " rebootq
+    read -p "Do you want some Nvidia? This installs cuda for Hashcat. (y/N) " nvidiaq
+    read -p "Do you want to run lynis? (y/N) " lynisq
+    read -p "Do you want to install Google Chrome? (Y/n) " chromeq
+    read -p "Will you be using xrdp? (Y/n) " xrdpq
+    read -p "Do you want to randomize the machine name? (Y/n) " randoq
+    read -p "Do you want to roll keys? (y/N) " keyq
 
-### End Hostname randomizer ###
+    rollKeys
+    mscode
+    letsUpdate
+    apps
+        case $randoq in
+                [nN]) msg_ok "Skipping machine rename";;
+                *) rando;;
+        esac
+        case $chromeq in
+                [nN]) msg_ok "Skipping Google Chrome install";;
+                *) chromeInstall;;
+        esac
+        case $nvidiaq in
+                [yY]) nvidiaInstal;;
+                *) msg_ok "Skipping Nvidia install"l;;
+        esac
+        case $xrdpq in
+                [nN]) msg_ok "Skipping xrdp install";;
+                *) xrdpInstall;;
+        esac
+    tmuxStuff
+        case $lynisq in
+                [yY]) lynisrun;;
+                *) msg_ok "Skipping lynis run";;
+        esac
+    allDone
+}
 
-read -p "Would you like to restart now? (y/N) " yn
-case $yn in 
-    [yY] )sudo reboot;
-        break;;
-    * ) echo "you should restart soonest. Install app icons taskbar by visiting the following link: https://extensions.gnome.org/extension/4944/app-icons-taskbar/";
-        exit;;
+OPTIONS=(Full "Install (Apps etc, does not roll keys)" \
+         Custom "Install, lotsa options")
+
+CHOICE=$(whiptail --backtitle "Ubuntu Helper Scripts" --title "Install Packages" --menu "Select an option:" 10 58 2 \
+          "${OPTIONS[@]}" 3>&1 1>&2 2>&3)
+
+case $CHOICE in
+  "Full")
+    install
+    ;;
+  "Custom")
+    custom
+    ;;
+  *)
+    echo "Exiting..."
+    exit 0
+    ;;
 esac
