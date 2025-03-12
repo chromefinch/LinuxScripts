5938d9cd164a294a1b79e2a5dabde1583aae9bbb
chromefinch <38330372+chromefinch@users.noreply.github.com>
2025-03-12 15:29:33 -0400

    Update kali_Install.sh

--- kali_Install.sh
+++ kali_Install.sh
@@ -215,9 +215,9 @@ EOF
 
 mscode(){
     msg_info "Adding Microsoft Visual Studio Code repo"
-    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | $STD sudo gpg --dearmor > packages.microsoft.gpg
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor > packages.microsoft.gpg
     $STD sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-    echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | $STD sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+    echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
     $STD sudo rm -f packages.microsoft.gpg
     msg_ok "Visual Studio Code repo added"
 }
